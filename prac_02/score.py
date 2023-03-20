d6b8818b76726fa9412a48dd62847c1333409369
Kantapong14405427 <kantapong.wongsanguan@my.jcu.edu.au>
2023-03-20 14:31:44 +0800

    prac_02 update score.py with addition random a score

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,7 +2,7 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-
+import random
 MAX_SCORE = 100
 MIN_SCORE = 0
 PASS_SCORE = 50
@@ -13,6 +13,8 @@ def main():
     """Program to determine grade with the inout score"""
     score = float(input("Enter score: "))
     print(get_grade(score))
+    random_score = random.randint(0, 100)
+    print(f"A random score of {random_score} is generated. (a/an {get_grade(random_score)} grade)")
 
 
 def get_grade(score):
