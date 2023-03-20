1ff5cd9107515e533a1156bd97dfa9a28010a009
Kantapong14405427 <kantapong.wongsanguan@my.jcu.edu.au>
2023-03-20 14:24:38 +0800

    prac_02 update score.py with main and get_grade functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -7,12 +7,24 @@ MAX_SCORE = 100
 MIN_SCORE = 0
 PASS_SCORE = 50
 EXCELLENT_SCORE = 90
-score = float(input("Enter score: "))
-if score < MIN_SCORE or score > MAX_SCORE:
-    print("Invalid score")
-elif MIN_SCORE <= score < PASS_SCORE:
-    print("Bad")
-elif PASS_SCORE <= score < EXCELLENT_SCORE:
-    print("Passable")
-elif EXCELLENT_SCORE <= score <= MAX_SCORE:
-    print("Excellent")
+
+
+def main():
+    """Program to determine grade with the inout score"""
+    score = float(input("Enter score: "))
+    print(get_grade(score))
+
+
+def get_grade(score):
+    """Function to determine grade according to score"""
+    if score < MIN_SCORE or score > MAX_SCORE:
+        return "Invalid score"
+    elif MIN_SCORE <= score < PASS_SCORE:
+        return "Bad"
+    elif PASS_SCORE <= score < EXCELLENT_SCORE:
+        return "Passable"
+    elif EXCELLENT_SCORE <= score <= MAX_SCORE:
+        return "Excellent"
+
+
+main()
