f8d9c84eba75ca04881bff9bb2cebb87d3c6af1a
Kantapong14405427 <kantapong.wongsanguan@my.jcu.edu.au>
2023-04-10 15:26:55 +0800

    prac_05 add wimbledon.csv

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -16,6 +16,17 @@ def main():
     display_results(champion_to_count, countries)
 
 
+def get_records(filename):
+    """Get records from file in list of lists form."""
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Remove header
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    return records
+
+
 def process_records(records):
     """Create dictionary of champions and set of countries from records (list of lists)."""
     champion_to_count = {}
@@ -36,17 +47,6 @@ def display_results(champion_to_count, countries):
         print(name, count)
     print(f"\nThese {len(countries)} countries have won Wimbledon: ")
     print(", ".join(country for country in sorted(countries)))
-
-
-def get_records(filename):
-    """Get records from file in list of lists form."""
-    records = []
-    with open(filename, "r", encoding="utf-8-sig") as in_file:
-        in_file.readline()  # Remove header
-        for line in in_file:
-            parts = line.strip().split(",")
-            records.append(parts)
-    return records
 
 
 main()
