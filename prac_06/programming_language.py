68b92fbb821ea42b81f2e7e8773be8bbb18281ec
Kantapong14405427 <kantapong.wongsanguan@my.jcu.edu.au>
2023-04-24 13:31:12 +0800

    prac_06 done languages.py and programming_language.py

---
+++ prac_06/programming_language.py
@@ -0,0 +1,24 @@
+"""
+Estimated: 20 min
+Actual:
+"""
+
+
+class ProgrammingLanguage:
+    """Representing programming language class"""
+
+    def __init__(self, name, typing, reflection, year):
+        """Set parameters to pass in for programming language"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Display languages detail"""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Check if language typing is dynamic or not"""
+        return self.typing == "Dynamic"
+    
